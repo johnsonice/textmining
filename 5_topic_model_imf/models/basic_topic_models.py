79be288529383b0fcdcd61d110bf15f86b1e9bc2
Chengyu Huang <huangchengyu16@gmail.com>
2018-10-05 15:28:30 -0400

    update

--- 5_topic_model_imf/models/basic_topic_models.py
+++ 5_topic_model_imf/models/basic_topic_models.py
@@ -28,18 +28,25 @@ def prepare_data(save=True):
     docs = list()
     for paragraph in contents:
         docs.append([w for sentance in paragraph for w in sentance])
-    
-    #%%
+
     # build dictionary
-    if save:
-        dictionary = corpora.Dictionary(docs)
-        dictionary.filter_extremes(no_below=5,no_above=0.5, keep_n=10000)
-        # convert document into bow
-        corpus_bow = [dictionary.doc2bow(text) for text in docs]
-        ## comput tfidf feature vectors
-        tfidf = models.TfidfModel(corpus_bow) # smartirs = 'atc' https://radimrehurek.com/gensim/models/tfidfmodel.html
-        corpus_tfidf = tfidf[corpus_bow]
+
+    dictionary = corpora.Dictionary(docs)
+    dictionary.filter_extremes(no_below=5,no_above=0.5, keep_n=10000)
+    # convert document into bow
+    corpus_bow = [dictionary.doc2bow(text) for text in docs]
+    ## comput tfidf feature vectors
+    tfidf = models.TfidfModel(corpus_bow) # smartirs = 'atc' https://radimrehurek.com/gensim/models/tfidfmodel.html
+    corpus_tfidf = tfidf[corpus_bow]
     
+    ## save dictionary and corpora 
+    if save:
+        dictionary_save_path = '../data/dictionary.dict'
+        dictionary.compactify()
+        dictionary.save(dictionary_save_path)
+        corpora.MmCorpus.serialize('../data/corpus_bow.mm', corpus_bow)
+        corpora.MmCorpus.serialize('../data/corpus_tfidf.mm', corpus_tfidf)
+        #print(len(dictionary))
     return docs,dictionary,corpus_bow,corpus_tfidf
 
 #%%
@@ -72,7 +79,8 @@ def basic_lda(total_topics,corpus,dictionary,docs,score=False):
                               id2word = dictionary,
                               num_topics = total_topics,
                               alpha='auto',
-                              eta = 'auto')#,
+                              eta = 'auto',
+                              random_state = 2)#,
                               #workers = 20) #
                               #iterations = 1000,
     # Compute Coherence Score
@@ -127,6 +135,36 @@ def lsi(total_topics, corpus,dictionary,docs,score=False):
         return lsimodel,coherence_score
     return lsimodel
 
+def seeded_lda(total_topics,corpus,dictionary,docs,seed_topic_list, boost, score=False):
+    print('Modify beta prior ......')
+    _model = LdaModel(corpus = corpus_bow, id2word = dictionary,random_state = 2,alpha='auto',num_topics = total_topics,iterations=0)
+    beta_matrix = _model.expElogbeta
+    for t_id, st in enumerate(seed_topic_list):
+        for word in st:
+            try:
+                w_id = dictionary.token2id[word]
+                beta_matrix[t_id,w_id] = boost
+                print('{} : {} : {}'.format(t_id,w_id,word))
+            except:
+                continue
+    print('Training for {} documents ......'.format(len(corpus)))
+    seed_model = LdaModel(corpus = corpus_bow,
+                                  id2word = dictionary,
+                                  num_topics = total_topics,
+                                  eta = beta_matrix,
+                                  random_state=2)
+    # Compute Coherence Score
+    if score:
+        print('calculating coherence socre for {} documents ......'.format(len(docs)))
+        coherence_model_lda = CoherenceModel(model=seed_model, texts=docs, dictionary=dictionary, coherence='c_v')
+        coherence_lda = coherence_model_lda.get_coherence()
+        print('\nCoherence Score: ', coherence_lda)
+
+        return seed_model,coherence_lda
+    
+    return seed_model
+
+
 def fine_tune_lda(corpus, dictionary, texts, limit, start=2, step=2):
     """
     Compute c_v coherence for various number of topics
@@ -169,27 +207,52 @@ def fine_tune_lda(corpus, dictionary, texts, limit, start=2, step=2):
 if __name__== "__main__":
     
     save = True  ## save gensim objects, corpus, dictionary, and lda model
-    
+    mode = 'lda'
     docs,dictionary,corpus_bow,corpus_tfidf = prepare_data(save=save)
     
-    #model, score = basic_lda(total_topics=15,corpus=corpus_bow[:10000],dictionary=dictionary,docs=docs[:10000],score=True)
+    if mode == 'lda' or mode=='all':
+        n_topics = 25
+        model, score = basic_lda(total_topics=n_topics,corpus=corpus_bow[:10000],dictionary=dictionary,docs=docs[:10000],score=True)
+        print_topics_gensim(topic_model=model,
+                           total_topics = n_topics,
+                           num_terms=20,
+                           display_weights=True)
+    elif mode =='seed_lda' or mode=='all':
+        n_topics = 25
+        boost = 1000
+        seed_topic_list = [['mpm','MPM','CFM','cfm','ltv','LTC','DSTI','dsti','lcr','LCR',
+                            'capital_buffer','macroprudential','capital_flow','prudential'],
+                            ['population','ageing','pension','productivity','migration','migrat']]
+            
+        seed_model = seeded_lda(n_topics,corpus_bow,dictionary,docs,seed_topic_list, boost, score=False)
+        ## for some reason keeps buging out when calculating coherence score 
+        
+        print_topics_gensim(topic_model=seed_model,
+                           total_topics = n_topics,
+                           num_terms=20,
+                           display_weights=True)
     
-    model_list, coherence_values,n_topics = fine_tune_lda(dictionary=dictionary, corpus=corpus_bow,
-                                                        texts=docs, start=15, limit=35, step=1)
-    
-    best_model = model_list[np.argmax(coherence_values)]
-    best_topic_n = best_model.get_topics().shape[0]
-    
-    plt.plot(n_topics, coherence_values)
-    plt.show()
-    
-    print_topics_gensim(topic_model=best_model,
-                   total_topics = best_topic_n,
-                   num_terms=10,
-                   display_weights=True)
-    if save:
-        lda_model_filepath = '../data/lda_res'
-        best_model.save(lda_model_filepath)
+    elif mode == 'fine_tune' or mode =='all':
+        
+        model_list, coherence_values,n_topics = fine_tune_lda(dictionary=dictionary, corpus=corpus_bow,
+                                                            texts=docs, start=15, limit=35, step=1)
+        
+        best_model = model_list[np.argmax(coherence_values)]
+        best_topic_n = best_model.get_topics().shape[0]
+        
+        plt.plot(n_topics, coherence_values)
+        plt.show()
+        
+        print_topics_gensim(topic_model=best_model,
+                       total_topics = best_topic_n,
+                       num_terms=10,
+                       display_weights=True)
+        if save:
+            lda_model_filepath = '../data/lda_res'
+            best_model.save(lda_model_filepath)
+    else:
+        print('Invalide mode: {}'.format(mode))
+        pass
     
     
     
